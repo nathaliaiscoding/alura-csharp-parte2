a8757b6b0c2604eb397cbac3d7fc9759c1db748c
Nathalia <nathalialaudano@crazytechlabs.com.br>
2021-06-10 16:00:46 -0300

    finalizada parte sobre classe x objeto do curso 2

--- ByteBank/ContaCorrente.cs
+++ ByteBank/ContaCorrente.cs
@@ -3,5 +3,6 @@ public class ContaCorrente
     public string titular;
     public int agencia;
     public int conta;
-    public double saldo;
+    public double saldo = 100; // se não for atribuído nenhum valor, ele atribui 0 (zero)
+
 }
