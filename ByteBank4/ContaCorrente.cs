c752a70c60a0df34c05191bc7607588c93afc5aa
Nathalia Laudano <nathalialaudano@crazytechlabs.com.br>
2021-06-11 18:54:31 -0300

    construtor para conta corrente, contador de contas criadas e regras numero da agencia

---
+++ ByteBank4/ContaCorrente.cs
@@ -0,0 +1,71 @@
+namespace ByteBank4 {
+public class ContaCorrente
+{
+   public static int TotalDeContasCriadas { get; private set; }
+   
+    public Cliente Titular { get; set; }
+
+    private int _agencia;
+    public int Agencia { 
+        get {
+            return _agencia;
+        }
+         set {
+             if(value <= 0) {
+                 return;
+             }
+         }
+          }
+    public int Conta { get; set; }
+    private double _saldo = 100; // se não for atribuído nenhum valor, ele atribui 0 (zero)
+
+    public double Saldo {
+            get {
+                    return _saldo;
+                }
+                set {
+                    if (value < 0) {
+                        return;
+                    }
+                    _saldo = value;
+                }
+    }
+    public bool Sacar(double valor)
+    {
+        if (_saldo < valor)
+        {
+            return false;
+        }
+  
+        _saldo -= valor;
+        return true;
+  
+    }
+
+    public void Depositar(double valor)
+    {
+        _saldo += valor;
+    }
+
+    public bool Transferir(double valor, ContaCorrente contaDestino)
+    {
+
+        if (_saldo < valor)
+        {
+            return false;
+        }
+
+        _saldo -= valor;
+        contaDestino.Saldo += valor;
+        return true;
+    }
+   
+   // constructor
+   public ContaCorrente(int agencia, int conta) {
+       _agencia = agencia;
+       Conta = conta;
+
+       TotalDeContasCriadas++;
+   }
+}
+}
---
+++ ByteBank4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaNathalia = new ContaCorrente(856, 88909);
+            ContaCorrente contaThalita = new ContaCorrente(856, 88910);
+            ContaCorrente contaMarie = new ContaCorrente(856, 88911);
+
+            Console.WriteLine(contaNathalia.Agencia);
+            Console.WriteLine(contaNathalia.Conta);
+            Console.WriteLine(contaThalita.Agencia);        
+            Console.WriteLine(contaThalita.Conta);
+            Console.WriteLine(contaMarie.Agencia);
+            Console.WriteLine(contaMarie.Conta);
+        }
+    }
+}
