3d72db808e29800fdf03ac72b0a8c385f46a530e
Nathalia <nathalialaudano@crazytechlabs.com.br>
2021-06-11 12:06:28 -0300

    funcao sacar adicionada a classe conta corrente

--- ByteBank/ContaCorrente.cs
+++ ByteBank/ContaCorrente.cs
@@ -5,4 +5,17 @@ public class ContaCorrente
     public int conta;
     public double saldo = 100; // se não for atribuído nenhum valor, ele atribui 0 (zero)
 
+
+    public bool Sacar(double valor)
+    {
+        if (this.saldo < valor)
+        {
+            return false;
+        }
+        else
+        {
+            this.saldo -= valor;
+            return true;
+        }
+    }
 }
