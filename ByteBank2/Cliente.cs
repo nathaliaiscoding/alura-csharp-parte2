00b2b0a9c37a76c5fa4a44dbda47a66203d33adc
Nathalia <nathalialaudano@crazytechlabs.com.br>
2021-06-11 15:13:35 -0300

    criado bytebank 2

---
+++ ByteBank2/Cliente.cs
@@ -0,0 +1,7 @@
+namespace ByteBank {
+public class Cliente {
+    public string titular;
+    public string cpf;
+    public string profissao;
+}
+}
---
+++ ByteBank2/ContaCorrente.cs
@@ -0,0 +1,40 @@
+namespace ByteBank {
+public class ContaCorrente
+{
+    public Cliente titular;
+    public int agencia;
+    public int conta;
+    public double saldo = 100; // se não for atribuído nenhum valor, ele atribui 0 (zero)
+
+
+    public bool Sacar(double valor)
+    {
+        if (this.saldo < valor)
+        {
+            return false;
+        }
+  
+        this.saldo -= valor;
+        return true;
+  
+    }
+
+    public void Depositar(double valor)
+    {
+        this.saldo += valor;
+    }
+
+    public bool Transferir(double valor, ContaCorrente contaDestino)
+    {
+
+        if (this.saldo < valor)
+        {
+            return false;
+        }
+
+        this.saldo -= valor;
+        contaDestino.saldo += valor;
+        return true;
+    }
+}
+}
